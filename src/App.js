be1f6fc128e29f8f8662e7c776564982b197b3ba
Shaan3110 <hitarasigan0987654321@gmail.com>
2021-08-12 00:11:13 +0530

    Messages Part addition

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {ChatEngine} from 'react-chat-engine';
 import './App.css';
+import { ChatFeed } from './components/ChatFeed';
 
 function App() {
   return (
@@ -7,7 +8,8 @@ function App() {
     height="100vh"
     projectID="dd4fd4cd-1c2f-4621-9181-f860599298bb"
     userName="suchintandas"
-    userSecret="123"/>
+    userSecret="123"
+    renderChatFeed={(chatAppProps)=><ChatFeed {...chatAppProps} />}/>
   );
 }
 
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Messages from'./Messages'
+import MyMessages from'./MyMessages'
+import OthersMessages from'./OthersMessages'
+
+export const ChatFeed=(props)=> {
+    return (
+        <>
+            
+        </>
+    )
+}
